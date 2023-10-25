37eeb9b61ed49d9f20d2ca3b24d916b640560346
kohei3110 <kk.31108424@gmail.com>
2023-10-25 04:50:46 +0000

    add Event Grid trigger function in Python

--- src/Python/Api2/function_app.py
+++ src/Python/Api2/function_app.py
@@ -1,10 +1,26 @@
 import logging
 import azure.functions as func
-
+import json
 
 app = func.FunctionApp()
 
-@app.event_grid_trigger(arg_name="azeventgrid")
-def BlobStorageEventGrid(azeventgrid: func.EventGridEvent):
-    logging.info('Python EventGrid trigger processed an event')
-
+@app.function_name(name="eventGridTrigger")
+@app.event_grid_trigger(arg_name="event")
+@app.cosmos_db_output(arg_name="document",
+                      database_name="items", 
+                      container_name="events", 
+                      collection_name="events", 
+                      create_if_not_exists=True,
+                      partition_key="/id",
+                      connection="CosmosConnectionString",
+                      connection_string_setting="CosmosConnectionString")
+def BlobStorageEventGrid(event: func.EventGridEvent, document: func.Out[func.Document]):
+    result = json.dumps({
+        'id': event.id,
+        'data': event.get_json(),
+        'topic': event.topic,
+        'subject': event.subject,
+        'event_type': event.event_type,        
+    })
+    document.set(func.Document.from_json(result))
+    logging.info('Python EventGrid trigger processed an event: %s', result)
